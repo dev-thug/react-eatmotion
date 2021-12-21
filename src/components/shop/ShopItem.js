b0c2724c8a5d9b7eb5ea0f449d695a445fee34a5
HJ <de0978@gmail.com>
2021-12-21 23:21:25 +0900

    update 음식점 키값 출력 수정

--- src/components/shop/ShopItem.js
+++ src/components/shop/ShopItem.js
@@ -1,33 +1,38 @@
 import classes from "./Shop.module.css";
 
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Table,
-  Row,
-  Col,
-  Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardTitle,
+    Table,
+    Row,
+    Col,
+    Button,
 } from "reactstrap";
+import {Link} from "react-router-dom";
 
 function ShopItem(props) {
-  return (
-    // #ffcc66
-    <div className={classes["shop-item"]}>
-      <Row>
-          {/* <p>{props.id}</p> */}
-          <Col md="8">
-            <strong>{props.name}</strong>
-            <p>{props.address}</p>
-          </Col>
-          <Col md="3">
-            {/* <a href="/reserve-make">예약&nbsp;&nbsp;&nbsp;</a> */}
-            <Button color="warning">예약&nbsp;&nbsp;&nbsp;</Button>
-          </Col>
-      </Row>
-    </div>
-  );
+
+    const url = "/newreserve/"+props.id
+
+    console.log(props.id)
+    return (
+        // #ffcc66
+        <div className={classes["shop-item"]} key={props.key}>
+            <Row>
+                {/* <p>{props.id}</p> */}
+                <Col md="8">
+                    <strong>{props.name}</strong>
+                    <p>{props.address}</p>
+                </Col>
+                <Col md="3">
+                    {/* <a href="/reserve-make">예약&nbsp;&nbsp;&nbsp;</a> */}
+                    <Link to={url}><Button color="warning">예약&nbsp;&nbsp;&nbsp;</Button></Link>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default ShopItem;
