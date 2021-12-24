79ae93b8d7c14a3add7374ed799e5f62477e6778
Jongbin Lim <rkwk0515@gmail.com>
2021-12-24 09:48:47 +0900

    add Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,28 +1,17 @@
+import classes from "./Footer.module.css"
+
 
 
 const Footer = () => {
     return (
         <footer>
-            {/*<div id="footer_div">*/}
-            {/*    <p class="e">Eatmotion</p>*/}
-            {/*    <p>Team. 맛집을 구해조</p>*/}
-            {/*    <a> Phone: +82) 10-1234-5678</a>*/}
-            {/*    <a class="settle">|</a>*/}
-            {/*    <a>Email: MCL1@multicampus.com</a>*/}
-            {/*</div>*/}
-
-            {/*<div class="container-fluid px-0 bg-my py-3">*/}
-            {/*    <div class="row no-gutters">*/}
-            {/*        <div class="col-md-12 text-center">*/}
-            {/*            <p class="mb-0">*/}
-            {/*                Eatmotion &copy;*/}
-            {/*                2021*/}
-            {/*                APP | Project of <a href="#" target="_blank">맛집을 구해조</a>*/}
-            {/*            </p>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-        </footer>
+		<div className={classes.footer_div}>
+			<p className={classes.e}>Eatmotion</p>
+			<p>Team. 맛집을 구해조</p>
+			<a> Phone: +82) 10-1234-5678</a> <a className={classes.settle}>|</a> <a>Email: MCL1@multicampus.com</a>
+		</div>
+		<div className={classes.footer_bottom_div}>Eatmotion 2021 APP | Project of 맛집을 구해조</div>
+	</footer>
     )
 
 }
